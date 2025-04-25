171f84dcfc3a46fc53949c3ffdcf9e4574fdf2bc
Inbaselvan-ayyanar <141208152+Inbaselvan-ayyanar@users.noreply.github.com>
2025-04-25 21:32:10 +0530

    Update create qrcode.py

--- create qrcode.py
+++ create qrcode.py
@@ -4,12 +4,12 @@ import cv2
 
 qr= qrcode.QRCode(version=1, box_size=10, border=5)
 
-data1 = input()
-data2 = input()
-data=data1+" "+data2
+data = input()
 qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill_color="blue", back_color="white")
-path=data2+".png"
+path=data+".png"
 img.save(path)
-
+cv2.imshow("QRCode",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
